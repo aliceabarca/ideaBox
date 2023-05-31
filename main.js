100642d8cedde5e01a3145c3d44532d536ba50e1
Scotty Brown <scottybrown98@gmail.com>
2023-05-31 14:41:14 -0700

    Establish our data model skeleton

--- main.js
+++ main.js
@@ -7,28 +7,26 @@ var bodyInput = document.querySelector('#input-2');
 // event listener
 saveButton.addEventListener('click', function(event) {
   event.preventDefault();
-  captureIdea;
+  createIdeaObject(titleInput.value, bodyInput.value)
 }
 );
 
 // global variables
-
+var currentIdea;
 var ideaBoxArray = [];
 
 // functions
 
 function captureIdea(title, body) {
-  var ideaBox = {
+  return {
     title: title,
     body: body,
     id: Date.now()
   }
-  return ideaBox;
 }
 
-function createIdeaObject() {
-ideaBox.title = titleInput.value
-ideaBox.body = bodyInput.value
-console.log('hello')
-ideaBoxArray.push(ideaBox);
+function createIdeaObject(title, body) {
+currentIdea = captureIdea(title, body)
+ideaBoxArray.push(currentIdea)
+return currentIdea
 }
